e26acf14af51f483fa75b78f31c8de5eef881702
kaynaGo <96072255+kaynaGo@users.noreply.github.com>
2022-01-24 18:08:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var notas = [35,38,42,45,43,34,46,41,48,32];
+
+for (var i; i<marks.length; i++){
+    if () {
+      
+    }
+}
+
+function setup() 
+{
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
